30fbbc41fdcd303aba84bbfe378a2754bb62e72c
zsviczian <viczian.zsolt@gmail.com>
2025-06-14 13:02:50 +0200

    fix history

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -4319,6 +4319,7 @@ class App extends React.Component<AppProps, AppState> {
       const { elements, appState, collaborators, captureUpdate } = sceneData;
 
       if (captureUpdate) {
+        const nextElements = elements ? elements : undefined;
         const observedAppState = appState
           ? getObservedAppState({
               ...this.store.snapshot.appState,
@@ -4328,7 +4329,7 @@ class App extends React.Component<AppProps, AppState> {
 
         this.store.scheduleMicroAction({
           action: captureUpdate,
-          elements: elements ?? [],
+          elements: nextElements,
           appState: observedAppState,
         });
       }
