d009434ed931113c47e79223d63f9c2207c443a2
Diego Mateos <dimateos@ucm.es>
2026-06-13 19:10:18 +0200

    fix drop between frames bug

--- packages/element/src/frame.ts
+++ packages/element/src/frame.ts
@@ -580,7 +580,8 @@ export const addElementsToFrame = <T extends ElementsMapOrArray>(
       continue;
     }
 
-    if (element.frameId && element.frameId !== frame.id) {
+    // skip elements already in target frame
+    if (element.frameId && element.frameId === frame.id) {
       continue;
     }
 
